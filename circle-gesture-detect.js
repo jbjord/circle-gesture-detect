b90ac93c4a30be8d30c8786be3bd366305490f21
jbjord <jbjord@users.noreply.github.com>
2026-04-08 11:29:20 -0400

    Create circle-gesture-detect.js

---
+++ circle-gesture-detect.js
@@ -0,0 +1,19 @@
+
+
+class PointSample {
+    constructor(x, y, t) {
+        this.x = x;
+        this.y = y;
+        this.t = t;
+    }
+
+    /**
+     * Return Euclidean distance between two points
+     * @param {PointSample} other - the other point for comparison
+     * @returns {number} Euclidean distance
+     */
+    distance(other) {
+        return Math.sqrt((other.x - this.x)**2 + (other.y - this.y)**2)
+    }
+
+}
