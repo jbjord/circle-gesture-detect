ea3cc51d0e857b206578b0effe3595032cb4e961
jbjord <jbjord@users.noreply.github.com>
2026-04-09 09:15:41 -0400

    Create PointSample module

---
+++ src/model/point-sample.js
@@ -0,0 +1,32 @@
+/**
+ * A sampled xy-point with a timestamp
+ * @class
+ */
+class PointSample {
+    /**
+     * @constructor
+     * @param {number} x - X coordinate
+     * @param {number} y - Y coordinate
+     * @param {number} t - Timestamp (ms or arbitrary units)
+     */
+    constructor(x, y, t) {
+        /** @type {number} */
+        this.x = x;
+
+        /** @type {number} */
+        this.y = y;
+
+        /** @type {number} */
+        this.t = t;
+    }
+
+    /**
+     * Return Euclidean distance between this point and another.
+     * @param {PointSample} other - The other point for comparison.
+     * @returns {number} Euclidean distance.
+     */
+    distance(other) {
+        return Math.hypot(other.x - this.x, other.y - this.y)
+    }
+
+}
