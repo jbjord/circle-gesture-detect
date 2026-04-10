180f041939b81d970e3b0cd4a420d385f1afdee0
jbjord <jbjord@users.noreply.github.com>
2026-04-09 20:12:08 -0400

    Add helper functions

--- src/recognizer/circle-gesture-recognizer.js
+++ src/recognizer/circle-gesture-recognizer.js
@@ -12,10 +12,6 @@ export default class CircleGestureRecognizer {
         "notCircle"
     ];
 
-    //thresholds and parameters
-    #dejitterDistance;
-    #minDiameter;
-    #maxDiameter;
 
     /**
      * 
@@ -26,17 +22,26 @@ export default class CircleGestureRecognizer {
         minDiameter,
         maxDiameter
     } = {}) {
-        this.#dejitterDistance = dejitterDistance;
-        this.#minDiameter = minDiameter;
-        this.#maxDiameter = maxDiameter;
+        this.dejitterDistance = dejitterDistance;
+        this.minDiameter = minDiameter;
+        this.maxDiameter = maxDiameter;
 
+        /**@type {SampleLog|null} */
         this.log = null
         this.state = this.states.idle;
     }
 
     states = {
         idle: {
-
+            /**
+             * Start sampling
+             * @param {CircleGestureRecognizer} ctx - Context
+             * @param {PointSample} point - Starting point.
+             */
+            start(ctx, point) {
+                ctx.log = new SampleLog(point, ctx.dejitterDistance);
+                ctx.state = ctx.states.tooEarly;
+            }
         },
 
         tooEarly: {
@@ -60,5 +65,57 @@ export default class CircleGestureRecognizer {
         }
     }
 
+ 
 
+    /**
+     * Checks if gesture is within the minimum allowed diameter.
+     * Note this check is expected to be used for the transition out of tooEarly.
+     * @param {CircleGestureRecognizer} ctx - Context.
+     */
+    isWithinStartingCircle(ctx) {
+        return Math.abs(ctx.log.distanceFromStart()) <= ctx.minDiameter;
+    }
+
+    /**
+     * Checks if the gesture has exceeded the maximum allowed diameter.
+     * @param {CircleGestureRecognizer} ctx 
+     * @returns {boolean}
+     */
+    isTooBig(ctx) {
+        const dx = ctx.log.getBoundingWidth();
+        const dy = ctx.log.getBoundingHeight();
+        return dx > ctx.maxDiameter || dy > ctx.maxDiameter;
+    }
+
+    /**
+     * Change state to notCircle.
+     * Can happen from any state except idle & tooEarly.
+     */
+    #toNotCircle() {
+        this.state = this.states.notCircle;
+    }
+
+    /**
+     * @todo
+     * @returns {boolean}
+     */
+    #looksRoughlyCircular() {
+        return true;
+    }
+
+    /**
+     * @todo
+     * @returns {boolean}
+     */
+    #stillLooksCircular() {
+        return true;
+    }
+
+    /**
+     * @todo
+     * @returns {boolean}
+     */
+    #circularEvidenceStrong() {
+        return true;
+    }
 }
